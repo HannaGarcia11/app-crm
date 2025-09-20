62df25baa4bed5fb1265fa791f883b4047febbd0
Hanna Garcia Guerra <hnngarcia11@gmail.com>
2025-09-20 14:50:24 -0500

    (develop): Validación de formulario con expresiones regulares

---
+++ helpers/expresiones.js
@@ -0,0 +1,4 @@
+export let expresiones = {
+    texto : /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{5,50}$/,
+    numero: /^[0-9]{5,10}$/
+}
--- helpers/validarResgitro.js
+++ helpers/validarResgitro.js
@@ -3,15 +3,47 @@
     querySelector sivre para obtener un elemento por su selector CSS
     querySelectorAll sirve para obtener todos los elementos que coincidan con el selector CSS
 */
+import {expresiones} from "./expresiones.js"
 
-
-    let inputs = document.getElementByClassName("formulario__entrada")
+    let inputs = document.getElementsByClassName("formulario__entrada")
     let arregloInputs = [...inputs] // Convierte una colección HTML en un array - Los ... son el operador spread que sirve para expandir los elementos de un iterable (como un array o una cadena) en lugares donde se esperan múltiples elementos.
-    console.log(inputs)
-    console.log(arregloInputs)
+    
+    arregloInputs.map((input)=> {
+        input.addEventListener("keyup", (e)=>{
+            switch(e.target.name){
+                case "nombre":
+                    if(expresiones.texto.test(e.target.value)){
+                        e.target.style.borderColor = "green" // e.target es el elemento que disparó el evento, como ya sabemos que elemento estamos manipulando, simplemente usamos e.target
+                    } else{
+                        document.getElementById("nombre").style.borderColor = "red" // document.getElementById("nombre") es el elemento con ID nombre, se especifica que elemento esta manipulando
+                    }
+                    break;
+                case "apellido":
+                    if(expresiones.texto.test(e.target.value)){
+                        e.target.classList.add("correcto") // e.targe me selecciona la etiqueda y el classList.add() me permite agregar una clase
+                        e.target.classList.remove("incorrecto") // e.targe me selecciona la etiqueda y el classList.remove() me permite eliminar una clase
+                    } else {
+                        e.target.classList.add("incorrecto")
+                        e.target.classList.remove("correcto")
+                    }
+                    break;
+                case "documento":
+                    if(expresiones.numero.test(e.target.value)){
+                        e.target.style.borderColor = "green"
+                    } else{
+                        e.target.style.borderColor = "red"
+                    }
+                    break;
+                case "telefono":
+                    if(expresiones.numero.test(e.target.value)){
+                        e.target.classList.add("correcto")
+                        e.target.classList.remove("incorrecto")
+                    } else {
+                        e.target.classList.add("incorrecto")
+                        e.target.classLista.remove("correcto")
+                    }
+                    break;
 
-
-
-arregloInputs.map((input)=> {
-    console.log(input)
+            }
+        })
 });
